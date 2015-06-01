267dfe8b6b73984337f7aec5e27a47add92aca81
spunkpike <spunkpike@gmail.com>
2015-06-01 06:50:41 +0530

    nav-bar working perfectly

--- New/js/creative.js
+++ New/js/creative.js
@@ -5,44 +5,51 @@
  */
 
 (function($) {
-    "use strict"; // Start of use strict
+  "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
+  });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    })
+  // Highlight the top nav as scrolling occurs
+  $('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: 51
+  })
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
+  // Closes the Responsive Menu on Menu Item Click
+  // $('.navbar-collapse li a ul li a').click(function() {
+  //     $('.navbar-toggle:visible').click();
+  // });
 
-    // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
+  // Custom JS snippet added by me for closing the menu
+  $(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+      $(this).collapse('hide');
+    }
+  });
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+  // Fit Text Plugin for Main Header
+  $("h1").fitText(
+    1.2, {
+      minFontSize: '35px',
+      maxFontSize: '65px'
+    }
+  );
 
-    // Initialize WOW.js Scrolling Animations
-    new WOW().init();
+  // Offset for Main Navigation
+  $('#mainNav').affix({
+    offset: {
+      top: 100
+    }
+  })
+
+  // Initialize WOW.js Scrolling Animations
+  new WOW().init();
 
 })(jQuery); // End of use strict
