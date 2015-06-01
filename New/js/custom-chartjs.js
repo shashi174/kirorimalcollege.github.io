c0480f9df662cd6cb3908c0ce44cee07b126e086
spunkpike <spunkpike@gmail.com>
2015-06-02 01:17:02 +0530

    some minor changes made

--- New/js/custom-chartjs.js
+++ New/js/custom-chartjs.js
@@ -300,25 +300,25 @@
 
 
   // bar chart data
-  var barData = {
-    labels : ["January","February","March","April","May","June"],
-    datasets : [
-      {
-        fillColor : "#48A497",
-        strokeColor : "#48A4D1",
-        data : [456,479,324,569,702,600]
-      },
-      {
-        fillColor : "rgba(73,188,170,0.4)",
-        strokeColor : "rgba(72,174,209,0.4)",
-        data : [364,504,605,400,345,320]
-      }
-    ]
-  }
-  // get bar chart canvas
-  var income = document.getElementById("income").getContext("2d");
-  // draw bar chart
-  new Chart(income).Bar(barData);
+  // var barData = {
+  //   labels : ["January","February","March","April","May","June"],
+  //   datasets : [
+  //     {
+  //       fillColor : "#48A497",
+  //       strokeColor : "#48A4D1",
+  //       data : [456,479,324,569,702,600]
+  //     },
+  //     {
+  //       fillColor : "rgba(73,188,170,0.4)",
+  //       strokeColor : "rgba(72,174,209,0.4)",
+  //       data : [364,504,605,400,345,320]
+  //     }
+  //   ]
+  // }
+  // // get bar chart canvas
+  // var income = document.getElementById("income").getContext("2d");
+  // // draw bar chart
+  // new Chart(income).Bar(barData);
 
 
   // Custom chart Js code ends here
@@ -338,9 +338,8 @@
     ctx1();
     ctx2();
     ctx3();
-    ctx4();
+    // ctx4();
   };
-
 
   var pieOptions = {
     segmentShowStroke : false,
