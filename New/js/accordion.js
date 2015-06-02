bdf31412140afcd4b1de7952a9c1d8387ee8ee1c
spunkpike <spunkpike@gmail.com>
2015-06-02 20:08:49 +0530

    add faqs

---
+++ New/js/accordion.js
@@ -0,0 +1,23 @@
+$(function () {
+
+    var active = true;
+
+    $('#collapse-init').click(function () {
+        if (active) {
+            active = false;
+            $('.panel-collapse').collapse('show');
+            $('.panel-title').attr('data-toggle', '');
+            $(this).text('Enable accordion behavior');
+        } else {
+            active = true;
+            $('.panel-collapse').collapse('hide');
+            $('.panel-title').attr('data-toggle', 'collapse');
+            $(this).text('Disable accordion behavior');
+        }
+    });
+
+    $('#accordion').on('show.bs.collapse', function () {
+        if (active) $('#accordion .in').collapse('hide');
+    });
+
+});
