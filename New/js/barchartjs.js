265d65e8d7ea3d7e2f6d10c02388414c2ea0765e
spunkpike <spunkpike@gmail.com>
2015-06-07 19:43:16 +0530

    change bar chart colours

--- New/js/barchartjs.js
+++ New/js/barchartjs.js
@@ -31,8 +31,8 @@ var $gplus = "#dc4937";
       $(".chartJS").height("");
       $(".chartJS").each(function(i,el){ m = Math.max(m,$(el).height()); });
       $(".chartJS").height(m);
-      }, 30);
-    }
+    }, 30);
+  }
   $(window).on('resize', function(){ size(false); });
 
   function redraw(animation){
@@ -40,9 +40,9 @@ var $gplus = "#dc4937";
 
     };
     if (!animation){
-        options.animation = false;
+      options.animation = false;
     } else {
-        options.animation = true;
+      options.animation = true;
     }
 
     // Custom chart Js code starts here
@@ -52,16 +52,21 @@ var $gplus = "#dc4937";
       labels : ["January","February","March","April","May","June","July"],
       datasets : [
         {
-          fillColor : $dark_blue,
-          strokeColor : $dark_blue,
-          data : [65,59,90,81,56,55,40]
+          label: "My First dataset",
+          fillColor: "rgba(220,220,220,0.5)",
+          strokeColor: "rgba(220,220,220,0.8)",
+          highlightFill: "rgba(220,220,220,0.75)",
+          highlightStroke: "rgba(220,220,220,1)",
+          data: [65, 59, 80, 81, 56, 55, 40]
         },
         {
-          fillColor : $gplus,
-          strokeColor : $gplus,
-          data : [28,48,40,19,96,27,100]
-        }
-      ]
+          label: "My Second dataset",
+          fillColor: "rgba(151,187,205,0.5)",
+          strokeColor: "rgba(151,187,205,0.8)",
+          highlightFill: "rgba(151,187,205,0.75)",
+          highlightStroke: "rgba(151,187,205,1)",
+          data: [28, 48, 40, 19, 86, 27, 90]
+        }      ]
     }
     var myLine = new Chart(document.getElementById("barChart").getContext("2d")).Bar(barChartData);
 
@@ -161,41 +166,41 @@ var $gplus = "#dc4937";
 
 
     // 6. Doughnut
- //    var doughnutData = [
- // {
- //      value: 300,
- //      color:"#F7464A",
- //      highlight: "#FF5A5E",
- //      label: "Red"
- //    },
- //    {
- //      value: 50,
- //      color: "#46BFBD",
- //      highlight: "#5AD3D1",
- //      label: "Green"
- //    },
- //    {
- //      value: 100,
- //      color: "#FDB45C",
- //      highlight: "#FFC870",
- //      label: "Yellow"
- //    },
- //    {
- //      value: 40,
- //      color: "#949FB1",
- //      highlight: "#A8B3C5",
- //      label: "Grey"
- //    },
- //    {
- //      value: 120,
- //      color: "#4D5360",
- //      highlight: "#616774",
- //      label: "Dark Grey"
- //    }
+    //    var doughnutData = [
+    // {
+    //      value: 300,
+    //      color:"#F7464A",
+    //      highlight: "#FF5A5E",
+    //      label: "Red"
+    //    },
+    //    {
+    //      value: 50,
+    //      color: "#46BFBD",
+    //      highlight: "#5AD3D1",
+    //      label: "Green"
+    //    },
+    //    {
+    //      value: 100,
+    //      color: "#FDB45C",
+    //      highlight: "#FFC870",
+    //      label: "Yellow"
+    //    },
+    //    {
+    //      value: 40,
+    //      color: "#949FB1",
+    //      highlight: "#A8B3C5",
+    //      label: "Grey"
+    //    },
+    //    {
+    //      value: 120,
+    //      color: "#4D5360",
+    //      highlight: "#616774",
+    //      label: "Dark Grey"
+    //    }
 
- //    ];
+    //    ];
 
- //    var myDoughnut = new Chart(document.getElementById("doughnutChart").getContext("2d")).Doughnut(doughnutData);
+    //    var myDoughnut = new Chart(document.getElementById("doughnutChart").getContext("2d")).Doughnut(doughnutData);
 
     // Custom chart Js code ends here
 
